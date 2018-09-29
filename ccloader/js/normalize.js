818b693fb7d45ee0c793cde3e805b0bc510f2d8f
ac2pic <ac2pic@gmail.com>
2018-09-29 17:15:57 -0500

    s

--- ccloader/js/normalize.js
+++ ccloader/js/normalize.js
@@ -1,5 +1,3 @@
-if(!String.prototype.endsWith){
-	String.prototype.endsWith = function(end){
-		return this.substr(this.length - end.length, end.length) === end;
-	}
+String.prototype.endsWith = String.prototype.endsWith || function(end){
+	return this.substr(this.length - end.length, end.length) === end;
 }
