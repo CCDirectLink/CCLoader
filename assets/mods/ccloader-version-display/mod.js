627e31a54376cc2248521a4f4bac20e4a0d36762
ac2pic <ac2pic@gmail.com>
2019-04-24 12:53:01 -0500

    Fixed issues
    Made the code more clear. Removed redundant code. Better named variables.

--- assets/mods/ccloader-version-display/mod.js
+++ assets/mods/ccloader-version-display/mod.js
@@ -12,50 +12,53 @@
             DEFAULT: {
                 state: {},
                 time: 0.2,
-                p: KEY_SPLINES.EASE
+                [entries.keySpline] : KEY_SPLINES.EASE
             },
             HIDDEN: {
                 state: {
                     alpha: 0
                 },
                 time: 0.2,
-                p: KEY_SPLINES.LINEAR
+                [entries.keySpline]: KEY_SPLINES.LINEAR
             }
         };
 		return text;
 	}
-	const onModsLoaded = function() {
+	
+	document.body.addEventListener("modsLoaded", () => {
 		let titleBG = cc.ig.GUI.menues.filter((e) => e[entries.GUI] instanceof cc.sc.TitleScreenBG).pop();
 		if(!titleBG) {
 			throw new Error('Could not find titleBG');
 		}
-		let versionButton = createCCLoaderVersionText();
-		versionButton[entries.setGuiStateTransition]("HIDDEN", h);
+		let versionText = createCCLoaderVersionText();
+		versionText[entries.setGuiStateTransition]("HIDDEN", h);
 		
-		titleBG[entries.GUI][entries.addGui](versionButton);
-		let oldC = titleBG[entries.GUI][entries.observerCallback];
-		titleBG[entries.GUI][entries.observerCallback] = function(c,d) {
-			if(c == cc.sc.playerModelInstance && d === 0) {
+		titleBG[entries.GUI][entries.addGui](versionText);
+		
+		let oldGuiCallback = titleBG[entries.GUI][entries.observerCallback];
+		
+		titleBG[entries.GUI][entries.observerCallback] = function(object,eventCode) {
+			// eventCode === CHANGED_STATE
+			if(object == cc.sc.playerModelInstance && eventCode === 0) {
 				let currentTransition = cc.sc.playerModelInstance[entries.isMainMenu]() ? "DEFAULT" : "HIDDEN";
 				if(this[entries.GuiConfig][entries.currentTransition] != currentTransition) {
-					versionButton[entries.setGuiStateTransition]("HIDDEN");
+					versionText[entries.setGuiStateTransition]("HIDDEN");
 				}
 			}	
-			oldC.apply(this, arguments);
+			oldGuiCallback.apply(this, arguments);
 		}
 		
 
-		var oldCallback = titleBG[entries.GUI][entries.titleParallax][entries.callback];
+		let oldTitleCallback = titleBG[entries.GUI][entries.titleParallax][entries.callback];
 
-		titleBG[entries.GUI][entries.titleParallax][entries.callback] = function(a,b) {
-			
-			if(a === 1 && b === "IDLE") {
-				versionButton[entries.setGuiStateTransition]("DEFAULT");	
+		titleBG[entries.GUI][entries.titleParallax][entries.callback] = function(transitionState, transitionType) {
+			// transitionState === ADD (my guess)
+			if(transitionState === 1 && transitionType === "IDLE") {
+				versionText[entries.setGuiStateTransition]("DEFAULT");	
 			}
-			oldCallback.apply(this, arguments);
+			oldTitleCallback.apply(this, arguments);
 		}
-		document.body.removeEventListener("modsLoaded", onModsLoaded);
-	};
+	});
+	
 
-	document.body.addEventListener("modsLoaded", onModsLoaded);
 })()
