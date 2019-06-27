7591ee1994463b15c9671dff362711833b50a9c6
2767mr <2767mr@gmx.at>
2019-06-27 12:18:08 +0200

    Removed file that was commited by accident

--- ccloader/compare.js
+++
@@ -1,7 +0,0 @@
-//var c=document.createElement("script");document.body.appendChild(c),c.type="text/javascript",c.src="compare.js"
-
-var ac = require("acorn");
-var walker = require("acorn/dist/walk");
-
-var current = ac.parse(filemanager.getResource('assets/js/game.compiled.js'), {onToken: function(){}});
-var old = ac.parse(filemanager.getResource('assets/js/game.min.js'), {onToken: function(){}});
