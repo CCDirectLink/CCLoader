326ad3971b94adfaf05bbf623aa8a5baa45b18d5
ac2pic <ac2pic@gmail.com>
2019-09-22 13:45:13 -0500

    Added some comments to CustomDebugState

--- assets/mods/simplify/lib/custom-debug-state.js
+++ assets/mods/simplify/lib/custom-debug-state.js
@@ -1,10 +1,19 @@
 import {DebugState} from './patch-steps-es6.js';
 
 export default class CustomDebugState extends DebugState {
+	/**
+	 * Sets current patch for context url resolving
+	 * @param {{mod: Mod, path: string}} currentPatch 
+	 */
 	setPatch(currentPatch) {
 		this.currentPatch = currentPatch;
 	}
 
+	/**
+	 * Prints current context file stack.
+	 * @param {{path: string, stack: string[]}} file
+	 *  
+	 */
 	printFileInfo(file) {
 		// resolve the urls
 		let [protocol, path] = file.path.split(":");
