896639c95c57dc2d661902071c20ad1b670f2394
ac2pic <ac2pic@gmail.com>
2019-09-29 14:16:08 -0500

    Simplified Greenworks

--- ccloader/js/greenworks.js
+++ ccloader/js/greenworks.js
@@ -1,21 +1,6 @@
 export class Greenworks {
-	constructor() {
-		this._greenworks = null;
-
-		const version = process.versions["node-webkit"];
-		if (semver.lt(version, "0.14.0")) {
-			if (this.startedFromSteam()) {
-				this._greenworks = require('../assets/modules/greenworks-0.4.0/greenworks');
-			}
-		} else if (semver.lt(version, "0.30.0")) {
-			this._greenworks = require('../assets/modules/greenworks-0.5.3/greenworks');
-		} else {
-			this._greenworks = require('../assets/modules/greenworks-0.13.0/greenworks');	
-		}
-	}
-
-	startedFromSteam() {
-		return nw.App.argv.indexOf("--startedFromSteam") > -1;
+	constructor(version) {
+		this._greenworks = require(`../assets/modules/greenworks-${version}/greenworks`);
 	}
 
 	init() {
