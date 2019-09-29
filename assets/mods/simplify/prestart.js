af95c916326be167ba001d354d369e86c3d4aff4
ac2pic <ac2pic@gmail.com>
2019-09-29 13:58:57 -0500

    Injected Greenworks

---
+++ assets/mods/simplify/prestart.js
@@ -0,0 +1,29 @@
+ig.module("impact.feature.greenworks.greenworks-fix").requires("impact.feature.greenworks.greenworks").defines(function() {
+	ig.Greenworks.inject({
+		init: function() {
+			this.name = "Greenworks";
+			try {
+				this.greenworks = parent.makeGreenWork();
+				this.steps.push("loaded");
+	
+				const version = process.versions["node-webkit"];
+				if (semver.lt(version, "0.14.0")) {
+					if (this.startedFromSteam()) {
+						this.greenworks.initAPI();
+					}
+				} else {
+					this.greenworks.init();	
+				}
+			} catch (error) {
+				this.steps.push("error");
+				this.errorMsg = error.toString();
+				this.greenworks = null;
+			}
+
+			if (this.greenworks) {
+				this.steps.push("initialized");
+			}
+			
+		}
+	});
+});
