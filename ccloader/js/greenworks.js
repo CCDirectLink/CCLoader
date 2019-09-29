007c9443236e8a7590bf37052ca071c50b9747a4
ac2pic <ac2pic@gmail.com>
2019-09-29 14:04:49 -0500

    Added Greenworks class to CCLoader

---
+++ ccloader/js/greenworks.js
@@ -0,0 +1,40 @@
+export class GreenWorks {
+	constructor() {
+		this._greenworks = null;
+
+		const version = process.versions["node-webkit"];
+		if (semver.lt(version, "0.14.0")) {
+			if (this.startedFromSteam()) {
+				this._greenworks = require('../assets/modules/greenworks-0.4.0/greenworks');
+			}
+		} else if (semver.lt(version, "0.30.0")) {
+			this._greenworks = require('../assets/modules/greenworks-0.5.3/greenworks');
+		} else {
+			this._greenworks = require('../assets/modules/greenworks-0.13.0/greenworks');	
+		}
+	}
+
+	startedFromSteam() {
+		return nw.App.argv.indexOf("--startedFromSteam") > -1;
+	}
+
+	init() {
+		return this._greenworks.init();
+	}
+
+	initAPI() {
+		return this._greenworks.initAPI();
+	}
+	
+	clearAchievement(steamId, callback) {
+		this._greenworks.clearAchievement(steamId, function() {
+			return callback.apply(this, arguments);
+		});
+	}
+
+	activateAchievement(steamId, callback) {
+		this._greenworks.activateAchievement(steamId, function() {
+			return callback.apply(this, arguments);
+		});
+	}
+}
