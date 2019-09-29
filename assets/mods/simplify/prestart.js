190800ab7821abc20bc42ef96b16aea123594e8d
ac2pic <ac2pic@gmail.com>
2019-09-29 14:17:41 -0500

    Added proper versions

--- assets/mods/simplify/prestart.js
+++ assets/mods/simplify/prestart.js
@@ -5,15 +5,21 @@ ig.module("impact.feature.greenworks.greenworks-fix").requires("impact.feature.g
 			this.greenworks = null;
 			try {
 				const semver = parent.semver;
-				this.greenworks = new Greenworks();
+				
 				this.steps.push("loaded");
 	
 				const version = process.versions["node-webkit"];
 				if (semver.lt(version, "0.14.0")) {
 					if (this.startedFromSteam()) {
+						this.greenworks = new Greenworks('0.4.0');
 						this.greenworks.initAPI();
 					}
 				} else {
+					if (semver.lt(version, "0.30.0")) {
+						this.greenworks = new Greenworks('0.5.3');
+					} else {
+						this.greenworks = new Greenworks('0.13.0');
+					}
 					this.greenworks.init();	
 				}
 			} catch (error) {
