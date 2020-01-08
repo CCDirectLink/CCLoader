3e2d8bc086fb40ee8799ce37e66fe73a819ebd02
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-01-08 20:41:28 +0200

    put the URL overrider script into main.js

--- ccloader/js/main.js
+++ ccloader/js/main.js
@@ -1,4 +1,10 @@
+import './normalize.js';
 import { ModLoader } from './ccloader.js';
+
+if (window.process) {
+	var envVar = process.env.CCLOADER_OVERRIDE_MAIN_URL;
+	if (envVar) window.location.replace(envVar);
+}
 
 window.onload = () => {
 	const modloader = window.modloader = new ModLoader();
