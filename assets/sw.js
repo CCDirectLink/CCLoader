b7b82e103c78c53ad3d0f0389e1baa6dd11332c4
ac2pic <ac2pic@gmail.com>
2020-01-17 13:35:03 -0600

    Service worker now redirects to local server when it needs to

--- assets/sw.js
+++ assets/sw.js
@@ -1,10 +1,35 @@
-self.addEventListener('activate', (event) => {
+function splitPackedUrl(url) {
+    const tempUrl = new URL(url);
+    // stop at .ccmod
+    const len = tempUrl.pathname.lastIndexOf(".ccmod") + ".ccmod".length;
+    return {
+        basePath: tempUrl.pathname.substring(1, len),
+        relativePath: tempUrl.pathname.substring(len + 1)
+    };
+}
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting.bind(self));
+});
+
+self.addEventListener('active', function(event) {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function(event) {
-   
-    if (event.request.url.includes('mods/')) {
-        console.log(event.request)
-    }
+    const url = new URL(event.request.url);
 
+    if (event.request.method === "GET") {
+        if (url.pathname.includes('mods/api/preload')) {
+            const basePath = url.searchParams.get('path');
+            event.respondWith(fetch(`http://localhost:3000/mods/api/preload?path=${basePath}`));           
+        } else if (url.pathname.includes("mods/api/get-assets")) {
+            const basePath = url.searchParams.get('path');
+            event.respondWith(fetch(`http://localhost:3000/mods/api/get-assets?path=${basePath}`));
+        } else if (url.pathname.match(/mods\/[^\/]+.ccmod/)) {
+            const {basePath, relativePath} = splitPackedUrl(event.request.url);
+            event.respondWith(fetch(`http://localhost:3000/mods/api/get-asset?path=${basePath}&relativePath=${relativePath}`));
+    
+        }
+    }
 });
