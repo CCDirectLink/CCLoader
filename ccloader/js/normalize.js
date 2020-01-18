7a1117c60af8a8f66dc60ea52753f13b843a6a45
ac2pic <ac2pic@gmail.com>
2020-01-18 10:43:19 -0600

    Added normalize function to fake path

--- ccloader/js/normalize.js
+++ ccloader/js/normalize.js
@@ -7,7 +7,36 @@ if (!window.require) {
 		if (name === 'path') {
 			return {
 				sep: '/',
-				join: (...args) => args.join('/')
+				normalize: function (path) {
+
+					// convert all \ to / and remove all duplicate //
+
+					path = path.replace(/\\/g, this.sep).replace(/\/\//g, this.sep);
+					
+					// split by the path separator 
+					let pieces = path.split(this.sep);
+
+					// resolve all .. and .
+
+					for (let pieceIndex = 0; pieceIndex < pieces.length; pieceIndex++) {
+						const currPiece = pieces[pieceIndex];
+						
+						if (currPiece === '.' || (currPiece === '..' && pieceIndex === 0)) {
+							// remove the current entry
+							pieces.splice(pieceIndex, 1);
+							pieceIndex--;
+						} else if (currPiece === '..') {
+							// remove the current entry and previous entry
+							pieces.splice(pieceIndex - 1, 2);
+							pieceIndex -= 1;
+						}
+					}
+					return pieces.join(this.sep);
+
+				},
+				join: function(...args) {
+					return this.normalize(args.join(this.sep));
+				}
 			};
 		}
 		return undefined;
