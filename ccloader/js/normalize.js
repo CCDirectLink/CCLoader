4a0842fd60afa81ee9a27676627e930c64b09276
ac2pic <ac2pic@gmail.com>
2020-02-02 11:08:40 -0600

    Added posix property to path + made improvements to browser path module

--- ccloader/js/normalize.js
+++ ccloader/js/normalize.js
@@ -5,10 +5,41 @@ if (!window.require) {
 	window.isBrowser = true;
 	window.require = name => {
 		if (name === 'path') {
-			return {
+			const path = {
+				posix: path,
 				sep: '/',
-				join: (...args) => args.join('/')
+				normalize: function (path) {
+
+					// convert all \ to / and remove all duplicate //
+
+					path = path.replace(/\\/g, this.sep).replace(/\/\//g, this.sep);
+					
+					// split by the path separator 
+					let pieces = path.split(this.sep);
+
+					// resolve all .. and .
+
+					for (let pieceIndex = 0; pieceIndex < pieces.length; pieceIndex++) {
+						const currPiece = pieces[pieceIndex];
+						
+						if (currPiece === '.' || (currPiece === '..' && pieceIndex === 0)) {
+							// remove the current entry
+							pieces.splice(pieceIndex, 1);
+							pieceIndex--;
+						} else if (currPiece === '..') {
+							// remove the current entry and previous entry
+							pieces.splice(pieceIndex - 1, 2);
+							pieceIndex -= 1;
+						}
+					}
+					return pieces.join(this.sep);
+
+				},
+				join: function(...args) {
+					return this.normalize(args.join(this.sep));
+				}
 			};
+			return path;
 		}
 		return undefined;
 	};
