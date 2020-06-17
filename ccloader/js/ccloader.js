7872f7015d3f00740832a5ab413447f84fa5f69a
2767mr <2767mr@gmx.at>
2020-06-17 20:20:04 +0200

    Bumped patch version

--- ccloader/js/ccloader.js
+++ ccloader/js/ccloader.js
@@ -6,7 +6,7 @@ import { Loader } from './loader.js';
 import { Plugin } from './plugin.js';
 import { Greenworks } from './greenworks.js';
 
-const CCLOADER_VERSION = '2.18.4';
+const CCLOADER_VERSION = '2.18.5';
 
 export class ModLoader {
 	constructor() {
