13fa4b539afe29cd6b1e2a72b83d2cb25730d6b8
ac2pic <ac2pic@gmail.com>
2023-07-17 06:51:52 -0500

    Fix four

--- ccloader/js/ccloader.js
+++ ccloader/js/ccloader.js
@@ -110,6 +110,14 @@ export class ModLoader {
 	}
 
 
+	/**
+	 * Adds manifestPath to the proper array based upon its extension.
+	 * Ignored if no ending matches found.
+	 * @param {string} manifestPath
+	 * @param {string[]} modFiles grouped by /package.json ending
+	 * @param {string[]} ccmodFiles grouped by /ccmod.json ending
+	 * @param {string[]} packedMods grouped by .ccmod ending
+	 */
 	_sortModByExtension(manifestPath, modFiles, ccmodFiles, packedMods) {
 		const endings = this.filemanager.endings;
 		const selectedArray = [
